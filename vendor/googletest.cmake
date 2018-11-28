767a4a299345e820c49f437f01770be4bd5745e7
Andre Kohn <andre@kohn.io>
2018-11-28 09:16:11 +0100

    Fix gtest cmake config

--- vendor/googletest.cmake
+++ vendor/googletest.cmake
@@ -32,7 +32,7 @@ ExternalProject_Add(
         -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
     DOWNLOAD_COMMAND ""
     UPDATE_COMMAND ""
-    BUILD_BYPRODUCTS <INSTALL_DIR>/lib/libgmock.a
+    BUILD_BYPRODUCTS <INSTALL_DIR>/lib/libgtest.a
 )
 
 # Build gmock

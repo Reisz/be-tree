916b54cff9e2a5854e7f5641f16383c697b36cf3
Daniel Pfaff <daniel.pfaff@in.tum.de>
2018-12-10 10:11:44 +0100

    Port more tests.

--- test/buffer_manager_test.cc
+++ test/buffer_manager_test.cc
@@ -32,35 +32,35 @@ const std::vector<uint64_t> imlab::BufferManager::get_lru() const {
 namespace {
 // ---------------------------------------------------------------------------------------------------
 TEST(BufferManager, FixSingle) {
-    imlab::BufferManager buffer_manager{1024, 10};
+    imlab::BufferManager manager{1024, 10};
     std::vector<uint64_t> expected_values(1024 / sizeof(uint64_t), 123);
     {
-        auto fix = buffer_manager.fix_exclusive(1);
+        auto fix = manager.fix_exclusive(1);
         std::memcpy(fix.data(), expected_values.data(), 1024);
         fix.set_dirty();
         fix.unfix();
-        EXPECT_EQ(std::vector<uint64_t>{1}, buffer_manager.get_fifo());
-        EXPECT_TRUE(buffer_manager.get_lru().empty());
+        EXPECT_EQ(std::vector<uint64_t>{1}, manager.get_fifo());
+        EXPECT_TRUE(manager.get_lru().empty());
     }
     {
         std::vector<uint64_t> values(1024 / sizeof(uint64_t));
-        auto fix = buffer_manager.fix(1);
+        auto fix = manager.fix(1);
         std::memcpy(values.data(), fix.data(), 1024);
         fix.set_dirty();
         fix.unfix();
-        EXPECT_TRUE(buffer_manager.get_fifo().empty());
-        EXPECT_EQ(std::vector<uint64_t>{1}, buffer_manager.get_lru());
+        EXPECT_TRUE(manager.get_fifo().empty());
+        EXPECT_EQ(std::vector<uint64_t>{1}, manager.get_lru());
         ASSERT_EQ(expected_values, values);
     }
 }
 
 TEST(BufferManager, PersistentRestart) {
     {
-        imlab::BufferManager buffer_manager{1024, 10};
+        imlab::BufferManager manager{1024, 10};
         for (uint16_t segment = 0; segment < 3; ++segment) {
             for (uint64_t segment_page = 0; segment_page < 10; ++segment_page) {
                 uint64_t page_id = (static_cast<uint64_t>(segment) << 48) | segment_page;
-                auto fix = buffer_manager.fix_exclusive(page_id);
+                auto fix = manager.fix_exclusive(page_id);
                 uint64_t& value = *reinterpret_cast<uint64_t*>(fix.data());
                 value = segment * 10 + segment_page;
                 fix.set_dirty();
@@ -68,11 +68,11 @@ TEST(BufferManager, PersistentRestart) {
         }
     }
     {
-        imlab::BufferManager buffer_manager{1024, 10};
+        imlab::BufferManager manager{1024, 10};
         for (uint16_t segment = 0; segment < 3; ++segment) {
             for (uint64_t segment_page = 0; segment_page < 10; ++segment_page) {
                 uint64_t page_id = (static_cast<uint64_t>(segment) << 48) | segment_page;
-                auto fix = buffer_manager.fix(page_id);
+                auto fix = manager.fix(page_id);
                 uint64_t value = *reinterpret_cast<uint64_t*>(fix.data());
                 fix.unfix();
                 EXPECT_EQ(segment * 10 + segment_page, value);
@@ -80,6 +80,76 @@ TEST(BufferManager, PersistentRestart) {
         }
     }
 }
+
+TEST(BufferManager, FIFOEvict) {
+    imlab::BufferManager manager{1024, 10};
+    for (uint64_t i = 1; i < 11; ++i) {
+        auto fix = manager.fix(i);
+    }
+
+    {
+        std::vector<uint64_t> expected_fifo{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        EXPECT_EQ(expected_fifo, manager.get_fifo());
+        EXPECT_TRUE(manager.get_lru().empty());
+    }
+
+    {
+        auto fix = manager.fix(11);
+    }
+
+    {
+        std::vector<uint64_t> expected_fifo{2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        EXPECT_EQ(expected_fifo, manager.get_fifo());
+        EXPECT_TRUE(manager.get_lru().empty());
+    }
+}
+
+TEST(BufferManager, BufferFull) {
+    imlab::BufferManager manager{1024, 10};
+
+    std::vector<imlab::BufferFix> pages;
+    pages.reserve(10);
+
+    for (uint64_t i = 1; i < 11; ++i) {
+        auto fix = manager.fix(i);
+        pages.push_back(std::move(fix));
+    }
+
+    EXPECT_THROW(manager.fix(11), imlab::buffer_full_error);
+}
+
+TEST(BufferManager, MoveToLRU) {
+    imlab::BufferManager manager{1024, 2};
+
+    manager.fix(1);
+    manager.fix(2);
+
+    EXPECT_EQ((std::vector<uint64_t>{1, 2}), manager.get_fifo());
+    EXPECT_TRUE(manager.get_lru().empty());
+
+    manager.fix(2);
+
+    EXPECT_EQ(std::vector<uint64_t>{1}, manager.get_fifo());
+    EXPECT_EQ(std::vector<uint64_t>{2}, manager.get_lru());
+}
+
+TEST(BufferManager, LRURefresh) {
+    imlab::BufferManager manager{1024, 10};
+
+    manager.fix(1);
+    manager.fix(1);
+    manager.fix(2);
+    manager.fix(2);
+
+    EXPECT_TRUE(manager.get_fifo().empty());
+    EXPECT_EQ((std::vector<uint64_t>{1, 2}), manager.get_lru());
+
+    manager.fix(1);
+
+    EXPECT_TRUE(manager.get_fifo().empty());
+    EXPECT_EQ((std::vector<uint64_t>{2, 1}), manager.get_lru());
+}
+
 // ---------------------------------------------------------------------------------------------------
 }  // namespace
 // ---------------------------------------------------------------------------------------------------
